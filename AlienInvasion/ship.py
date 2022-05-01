b1989fe45bf094fd77c80bec4702d2ff842ff8d5
Jahirul Tusar <tusar86@gmail.com>
2022-05-01 16:24:22 +0100

    perma biug fixed of ship.bmp in ship.py

--- AlienInvasion/ship.py
+++ AlienInvasion/ship.py
@@ -10,7 +10,7 @@ class Ship:
 
         # Load the ship image and get its rect
         self.image = pygame.image.load(
-            "/home/meda/PycharmProjects/PythonCrashCourse/CrashCourseProjects/AlienInvasion/images/ship.bmp")
+            "images/ship.bmp")
         self.rect = self.image.get_rect()
 
         #Start each new ship at the bottom center of the screen
