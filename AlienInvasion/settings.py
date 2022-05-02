88eac397c9a4814e3c8d3caad06f6cb3b956c059
Jahirul Tusar <tusar86@gmail.com>
2022-05-02 12:42:59 +0100

    ship movement left right

--- AlienInvasion/settings.py
+++ AlienInvasion/settings.py
@@ -9,4 +9,8 @@ class Settings:
 
         self.screen_width = 1200
         self.screen_height = 800
-        self.bg_color = (230, 230, 230)
+        #self.bg_color = (230, 230, 230) #will change later
+        self.bg_color = (64, 61, 61) # Temporary color
+
+        # Snip settings
+        self.ship_speed = 1.5
