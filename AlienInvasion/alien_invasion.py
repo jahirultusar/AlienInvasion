fa70bce0730505f03828aada6abd31c2fb8de9a9
jahirultusar <41430709+jahirultusar@users.noreply.github.com>
2022-05-02 13:19:29 +0100

    add files via upload

--- AlienInvasion/alien_invasion.py
+++ AlienInvasion/alien_invasion.py
@@ -16,7 +16,7 @@ class AlienInvasion:
         
         self.screen = pygame.display.set_mode(
             (self.settings.screen_width, self.settings.screen_height))
-        pygame.display.set_caption("Alien Invasion 2022")
+        pygame.display.set_caption("Alien Invasion 2000")
 
         self.ship = Ship(self)
 
@@ -25,12 +25,31 @@ class AlienInvasion:
 
     def run_game(self):
         '''Start the main loop for the game'''
-        while True:
-            # watch for keyboard and mouse event
+        while True:            
+            self._check_events()
+            self.ship.update()
+            self._update_screen()
+
+    
+    def _check_events(self):
+        # watch for keyboard and mouse event
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_RIGHT:
+                        self.ship.moving_right = True
+                    elif event.key == pygame.K_LEFT:
+                        self.ship.moving_left = True
+                        
+                elif event.type == pygame.KEYUP:
+                    if event.key == pygame.K_RIGHT:
+                        self.ship.moving_right = False
+                    elif event.key == pygame.K_LEFT:
+                        self.ship.moving_left = False
 
+    
+    def _update_screen(self):
             # Redraw the screen during each pass through the loop
             self.screen.fill(self.settings.bg_color)
             self.ship.blitme()
