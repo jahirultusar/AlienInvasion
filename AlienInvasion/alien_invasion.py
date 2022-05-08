0b96948844c915e77074d8e429d41fb2a51da126
Jahirul Tusar <tusar86@gmail.com>
2022-05-08 13:54:32 +0100

    first aliens sighting! with alien.py

--- AlienInvasion/alien_invasion.py
+++ AlienInvasion/alien_invasion.py
@@ -7,6 +7,7 @@ import pygame
 from settings import Settings
 from ship import Ship
 from bullet import Bullet
+from alien import Alien
 
 class AlienInvasion:
     '''Overall class to manage game assets and behavior'''
@@ -22,6 +23,9 @@ class AlienInvasion:
 
         self.ship = Ship(self)
         self.bullets = pygame.sprite.Group()
+        self.aliens = pygame.sprite.Group()
+
+        self._create_fleet()
 
     def run_game(self):
         '''Start the main loop for the game'''
@@ -76,6 +80,12 @@ class AlienInvasion:
         if len(self.bullets) < self.settings.bullets_allowed:
             new_bullet = Bullet(self)
             self.bullets.add(new_bullet)
+
+    def _create_fleet(self):
+        """Create the fleet of aliens"""
+        #Make an alien
+        alien = Alien(self)
+        self.aliens.add(alien)
     
     def _update_screen(self):
             # Redraw the screen during each pass through the loop
@@ -83,6 +93,7 @@ class AlienInvasion:
             self.ship.blitme()
             for bullet in self.bullets.sprites():
                 bullet.draw_bullet()
+            self.aliens.draw(self.screen)
 
             # Make the most recently drawn screen visible.
             pygame.display.flip()
